0d2acc66dd31475c57aa28e4df853e4e38a74819
Nils VAN ZUIJLEN <nils.van-zuijlen@mailo.com>
2019-02-22 22:35:49 +0100

    debug

--- macros.py
+++ macros.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from dataclasses import dataclass
+from dataclasses import dataclass, astuple
 
 
 @dataclass()
@@ -61,7 +61,7 @@ class Macro:
         else:
             for row in panel:
                 for pixel in row:
-                    p.append(pixel.astuple())
+                    p.append(astuple(pixel))
         return p
 
     def __iter__(self):
